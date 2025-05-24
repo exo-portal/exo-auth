83d71799543cf1d83d16252a6cc60d0829d2b6e2
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-05-24 16:29:22 +0800

    feat: adding jwt login through email function

--- src/main/java/com/exodia_portal/auth/functions/auth/controller/AuthController.java
+++ src/main/java/com/exodia_portal/auth/functions/auth/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.exodia_portal.auth.functions.auth.controller;
 
+import com.exodia_portal.auth.functions.auth.dto.LoginRequestDto;
+import com.exodia_portal.auth.functions.auth.dto.RegisterRequestDto;
 import com.exodia_portal.auth.functions.auth.service.AuthService;
 import com.exodia_portal.auth.functions.jwt.service.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +20,33 @@ import java.util.Map;
 @RequestMapping("/authentication")
 public class AuthController {
 
+    @Autowired
     private AuthService authService;
 
+    @Autowired
     private JwtService jwtService;
+
+    /**
+     * Registers a new user and generates access and refresh tokens.
+     *
+     * @param request  the RegisterRequestDto containing user registration details
+     * @param response the HttpServletResponse object
+     * @return a ResponseEntity with the generated tokens
+     */
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> register(@RequestBody RegisterRequestDto request, HttpServletResponse response) {
+        return authService.register(request, response);
+    }
+
+    /**
+     * Handles user login by generating access and refresh tokens.
+     *
+     * @return a ResponseEntity with the generated tokens
+     */
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequestDto request, HttpServletResponse response) {
+        return authService.login(request, response);
+    }
 
     /**
      * Handles user logout by invalidating the session and clearing the JWT cookie.
@@ -31,16 +58,6 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<Map<String, String>> logout(HttpServletRequest request, HttpServletResponse response) {
         return authService.logout(request, response);
-    }
-
-    /**
-     * Handles user login by generating access and refresh tokens.
-     *
-     * @return a ResponseEntity with the generated tokens
-     */
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login() {
-        return authService.login();
     }
 
     /**
@@ -63,19 +80,6 @@ public class AuthController {
     @GetMapping("/get-security-token")
     public ResponseEntity<String> getToken(HttpServletRequest request) {
         return jwtService.getToken(request);
-    }
-
-    // TODO:: remove this method or move this in the jwt service
-
-    /**
-     * Generates a JWT token with the specified subject and expiration time.
-     *
-     * @param subject    the subject of the token
-     * @param expiration the expiration time in milliseconds
-     * @return the generated JWT token
-     */
-    private String generateToken(String subject, long expiration) {
-        return jwtService.generateToken(subject, expiration);
     }
 
 }
---
+++ src/main/java/com/exodia_portal/auth/functions/auth/dto/LoginRequestDto.java
@@ -0,0 +1,19 @@
+package com.exodia_portal.auth.functions.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+
+    private String email;
+
+    private String password;
+    
+}
+
