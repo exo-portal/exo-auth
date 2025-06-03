a38f4dfda7d7c0e6a2de0fdb9e1490809348fd1e
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-03 18:03:49 +0800

    feat: adding user role with jwt in login and register

--- src/main/java/com/exodia_portal/auth/functions/jwt/service/impl/JwtServiceImpl.java
+++ src/main/java/com/exodia_portal/auth/functions/jwt/service/impl/JwtServiceImpl.java
@@ -2,6 +2,7 @@ package com.exodia_portal.auth.functions.jwt.service.impl;
 
 import com.exodia_portal.auth.functions.jwt.service.JwtService;
 import com.exodia_portal.common.constant.ExoConstant;
+import com.exodia_portal.common.enums.AccessLevelTypeEnum;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.Cookie;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -60,6 +62,13 @@ public class JwtServiceImpl implements JwtService {
         return ResponseEntity.status(404).body("Token not found");
     }
 
+    /**
+     * Generates a JWT token with the specified subject and expiration time.
+     *
+     * @param subject the subject of the token (usually the user's email or ID)
+     * @param expiration the expiration time for the token in milliseconds
+     * @return a JWT token as a String
+     */
     @Override
     public String generateToken(String subject, long expiration) {
         Key key = new javax.crypto.spec.SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS256.getJcaName());
@@ -71,4 +80,30 @@ public class JwtServiceImpl implements JwtService {
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    /**
+     * Generates a JWT token with roles and features included in the payload.
+     *
+     * @param subject the subject of the token (usually the user's email or ID)
+     * @param currentRole the current role of the user
+     * @param roles a list of roles assigned to the user
+     * @param roleFeatureAccesses a list of features accessible by the user's roles
+     * @param expiration the expiration time for the token in milliseconds
+     * @return a JWT token as a String
+     */
+    @Override
+    public String generateTokenWithRolesAndFeatures(String subject, AccessLevelTypeEnum currentRole, List<String> roles, List<String> roleFeatureAccesses, long expiration) {
+        Key key = new javax.crypto.spec.SecretKeySpec(secretKey.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .claim("currentRole", currentRole)
+                .claim("roles", roles)
+                .claim("features", roleFeatureAccesses)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
 }
