ae899b970b8bd4926770ece01bbabe70c42546ab
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-03 23:44:52 +0800

    refactor: update return type for the register and login api

---
+++ src/main/java/com/exodia_portal/auth/functions/auth/dto/LoginResponseDto.java
@@ -0,0 +1,17 @@
+package com.exodia_portal.auth.functions.auth.dto;
+
+import com.exodia_portal.auth.functions.user.dto.UserResponseDto;
+import com.exodia_portal.common.enums.AccessLevelTypeEnum;
+import com.exodia_portal.common.model.User;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record LoginResponseDto(
+        UserResponseDto user,
+        List<String> featureKeys,
+        List<String> roleNames,
+        AccessLevelTypeEnum accessLevelRole) {
+
+}
